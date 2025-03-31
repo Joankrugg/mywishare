f00e3cd9992a98bbe40cd1d6c9fd9bfeb6729df2
Joankrugg <joan_krugg@live.fr>
2025-03-31 21:43:39 +0200

    get rid of video on assetsfrom precompiling

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,6 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join("app/assets/videos")
-Rails.application.config.assets.precompile += %w( millesimelife.mp4 )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
